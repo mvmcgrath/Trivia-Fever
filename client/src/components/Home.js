3648f2b6b7cebf6f66af74db16902dcaa38f279f
mm24642 <50781599+mm24642@users.noreply.github.com>
2022-09-19 12:54:35 -0400

    Fix home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,34 +1,77 @@
+import styled from 'styled-components'
+
 const Home = () => {
+  const BodyContainer = styled.div`
+    display: flex;
+    background-color: #A67DB8;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+    padding: 100px 0px;
+    color: white;
+  `
+
+  const BottomContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 50px;
+  `
+
+  const TextHeading = styled.h1`
+    font-size: 4em;
+    margin: 0px;
+  `
+
+  const Header = styled.header`
+    font-size: 3em;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+    margin: 0px;
+  `
+
   return(
-    <div>
-      <div>
-        Carousel?
-      </div>
-      <div>
-        <h1>Features</h1>
-      </div>
-      <div>
-        <table>
-          <tbody>
-            <tr>
-              <td>Play games of trivia with thousands of possible questions!</td>
-            </tr>
-            <tr>
-              <td>See detailed information about questions, including their percieved difficulty and category!</td>
-            </tr>
-            <tr>
-              <td>Create your own questions and games, that you can share with friends</td>
-            </tr>
-            <tr>
-              <td>Keep track of your trivia statistics, and see how you rank overall!</td>
-            </tr>
-            <tr>
-              <td>Become the best trivia player ever!</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
+    <BodyContainer>
+      <Header>
+        <TextHeading>Trivia Fever</TextHeading>
+        <p>The web browser trivia game anyone can play</p>
+      </Header>
+      <BottomContainer>
+        <div>
+          <div>
+            <h1>Features</h1>
+          </div>
+          <div>
+            <table>
+              <tbody>
+                <tr>
+                  <td>Play games of trivia with thousands of possible questions!</td>
+                </tr>
+                <tr>
+                  <td>See detailed information about questions, including their percieved difficulty and category!</td>
+                </tr>
+                <tr>
+                  <td>Create your own questions and games, that you can share with friends</td>
+                </tr>
+                <tr>
+                  <td>Keep track of your trivia statistics, and see how you rank overall!</td>
+                </tr>
+                <tr>
+                  <td>Become the best trivia player ever!</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div>
+          <div>
+            <h1>About</h1>
+          </div>
+        </div>
+      </BottomContainer>
+    </BodyContainer>
   )
 }
 
