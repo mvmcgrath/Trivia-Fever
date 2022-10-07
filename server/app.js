0627cc2be49a21ec0f01724b36a8d9cb2af49027
mm24642 <50781599+mm24642@users.noreply.github.com>
2022-10-07 11:59:55 -0400

    Add online web hosting

--- server/app.js
+++ server/app.js
@@ -23,6 +23,7 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
+app.use(express.static('build'))
 
 app.use('/api/questions', questionsRouter)
 app.use('/api/users', usersRouter)
